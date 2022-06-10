d45679f41b19ebe31020621cd2e71c3a2146fd00
duduxx123 <86330727+duduxx123@users.noreply.github.com>
2022-06-10 15:52:46 +0800

    Update User.java

--- Java/Spring5/day01/User.java
+++ Java/Spring5/day01/User.java
@@ -1,7 +1,27 @@
 package com.atguigu.spring5;
 
 public class User {
+    private String name;
+    private int num;
     public void add(){
         System.out.println("add......");
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public void prin() {
+        System.out.println(name);
+        System.out.println(num);
+    }
+
+    public User(String name, int num) {
+        this.name = name;
+        this.num = num;
     }
 }
